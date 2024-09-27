4b000c513b0234f7be534ef351bf62a6db3fd2b5
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-09-27 09:22:10 +0100

    update

--- lifecare/src/pages/Home.jsx
+++ lifecare/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import AnnounceLogo from "../doctor_home.jpeg"
+import AnnounceLogo from "../doctor_home.jpeg";
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GoogleIcon from '@mui/icons-material/Google';
+import PhoneForwardedIcon from '@mui/icons-material/PhoneForwarded';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 export function Home(){
 
     function Header(){
@@ -8,7 +16,7 @@ export function Home(){
                 <div className="row d-flex align-items-center" style={{
                   padding:30
                 }}>
-                  <div className="col-xl-4">
+                  <div className="col-xl-3">
                       <h3 style={{
                         color:"black",
                         fontWeight:"bold"
@@ -21,20 +29,35 @@ export function Home(){
                       fontWeight:"bold"
                     }}>
                     <div className="col" >
-                      <p>Home</p>
+                      <a href="" style={{
+                        textDecoration:"none",
+                        color:"#e7e5e4"
+                      }}>Home</a>
                     </div>
                     <div className="col">
-                      <p>Department</p>
+                      <a href="" style={{
+                        textDecoration:"none",
+                        color:"#e7e5e4"
+                      }}>Department</a>
                     </div>
                     <div className="col">
-                      <p>Doctors</p>
+                      <a href="" style={{
+                        textDecoration:"none",
+                        color:"#e7e5e4"
+                      }}>Doctors</a>
                     </div>
                     <div className="col">
-                      <p>Contact</p>
+                      <a href="" style={{
+                        textDecoration:"none",
+                        color:"#e7e5e4"
+                      }}>Contact</a>
                     </div>
                   </div>
                   <div className="col-xl-2">
-                    <button className="btn btn-primary">Make an appointment</button>
+                    <button className="btn " style={{
+                      backgroundColor: "#22d3ee",
+                      color:"white"
+                    }}>Make an appointment</button>
                   </div>
                 </div>
             </header>
@@ -44,7 +67,7 @@ export function Home(){
       return(
         <div className="row" style={{
           
-          padding:20  // Prevent the image from repeating
+          padding:30  // Prevent the image from repeating
         }}>
           <div className="row">
             <h1 style={{
@@ -71,10 +94,61 @@ export function Home(){
 
       )
     }
+    function Services(){
+      return(
+        <div className="row mt-4 mb-4">
+          <div className="col" style={{
+            backgroundColor:"#cffafe",
+            padding:20
+          }}>
+            <VaccinesIcon/>
+            <h4>Hospitality</h4>
+            <p>Clinical excellence must be the priority for any health care service provider</p>
+          </div>
+          <div className="col" style={{
+            backgroundColor:"#22d3ee",
+            padding:20
+          }}>
+            <AddIcCallIcon/>
+            <h4>Emergency Care</h4>
+            <p>Clinical excellence must be the priority for any health care service provider</p>
+          </div>
+          <div className="col" style={{
+            backgroundColor:"#cffafe",
+            padding:20
+          }}>
+            <LocalHospitalIcon/>
+            <h4>Chamber Service</h4>
+            <p>Clinical excellence must be the priority for any health care service provider</p>
+            <p></p>
+          </div>
+        </div>
+      )
+    }
+    function Footer(){
+      return(
+        <div className="row mt-4 ">
+          <div className="col">
+            <FacebookIcon/>
+            <LinkedInIcon/>
+            <GoogleIcon/>
+          </div>
+          <div className="col">
+            <p>&copy; {new Date().getFullYear()} EBooking. All rights reserved.</p>
+          </div>
+          <div className="col">
+            <PhoneForwardedIcon/>:+212 509090909
+            <MailOutlineIcon/>:lifecare.support@gmail.com
+          </div>
+        </div>
+      )
+    }
     return(
         <div className="row">
             <Header/>
             <Announce/>
+            <Services/>
+            <Footer/>
         </div>
     )
 }
